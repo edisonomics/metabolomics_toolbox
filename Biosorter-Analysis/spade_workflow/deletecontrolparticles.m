533f505525c34f569bb9051dd55f1649b899fc18
Laura C. Morris <lmorris@snowflake.attlocal.net>
2023-01-29 18:25:36 -0500

    Initial reorganization of Metabolomics Toolbox.

--- Biosorter-Analysis/spade_workflow/deletecontrolparticles.m
+++
@@ -1,6 +0,0 @@
-function [data] = deletecontrolparticles(data)
-load('intersecttest.mat');
-for z=1:length(intersectt);
-    inter = intersectt{z,1}';
-    data(:,inter) = []; 
-end
--- Biosorter-Analysis/spade_workflow/deleteintersectt.m
+++
@@ -1,5 +0,0 @@
-function [data] = deleteintersectt(data,intersectt)
-%intersectt = intersectt';
-for z=1:length(intersectt);
-    data(intersectt{z,1}) = []; 
-end
